93678ad407653cc1701893965a6d727ce40bdd89
Quartz <70311277+qrtzxv@users.noreply.github.com>
2025-01-03 04:01:15 +0800

    updated variables values, updated case commands, added fuzzel ouput dynamic line number, added power-profiles case

--- launchers/sysmenu/fuzzel-sysmenu.sh
+++ launchers/sysmenu/fuzzel-sysmenu.sh
@@ -3,49 +3,51 @@
 
 #date_d=$(date +"%Y %b %d")
 #date_t=$(date +"%H : %M : %S")
-dt="$(date +"%Y %b %d %a || %H : %M : %S")"
-focused_win=$(niri msg --json windows | jq '.[] | select(.is_focused == true)' | jq '.title')
-bat_pcent="Current: $(bat-man capacity)" ## comment out when not using a battery, change accordingly to battery daemon
-bat_max="Max:     $(bat-man threshold)" ##
-bat_stat="Status:  $(bat-man status)" ##
-vol_out="Vol: $(wpctl get-volume @DEFAULT_SINK@ | awk '{print $2, $3}')"
-vol_in="Mic: $(wpctl get-volume @DEFAULT_SOURCE@ | awk '{print $2, $3}')"
+dt=$(date +"%Y %b %d %a || %H : %M : %S")
+focused_win=$(niri msg --json windows | jq '.[] | select(.is_focused == true)' | jq '.title') ## insert whatever command it is for your window manager/compositor
+bat_pcent="Current: $(bat-man capacity)" ### comment out when not using a battery, change accordingly to battery daemon
+bat_max="Max: "$(bat-man threshold) ###
+bat_stat="Status: "$(bat-man status) ###
+vol_out="> Vol: "$(wpctl get-volume @DEFAULT_SINK@ | awk '{print $2, $3}') #### whatever audio service you're using
+vol_in="> Mic: "$(wpctl get-volume @DEFAULT_SOURCE@ | awk '{print $2, $3}') ####
 netwrx=$(nmcli -f TYPE,NAME connection show --active | grep -E 'ethernet|wlan|wifi' | head -n 1)
-ppc="Current: $(powerprofilesctl get)" ##
+pp="Current: "$(tuned-adm active | awk -F ': ' '{print $2}') ## current power profile
 cpu_pcent=$(top -bn1 | grep "Cpu(s)" | awk '{print 100 - $8}')
 cpu_temp=$(( $(cat /sys/class/thermal/thermal_zone0/temp) / 1000 ))
 ram_pcent=$(free | grep Mem | awk '{print ($3/$2)*100}')
 
 echo_str="$dt
 \n
-\nCurrect Focused Window ↴
+\nCurrect Focused Window ⌄
 \n$focused_win
 \n
-\n> Audio ↴
+\n> Audio ⌄
 \n$vol_out
 \n$vol_in
 \n
-\nBattery ↴
+\nBattery ⌄
 \n$bat_pcent / $bat_max
 \n$bat_stat
 \n
-\n> Network ↴
+\n> Network ⌄
 \n$netwrx
 \n
-\nPower Profile ↴
-\n$ppc
+\n> Power Profile ⌄
+\n$pp
 \n
 \nCPU Usage: $cpu_pcent %
 \nCPU Temp: $cpu_temp°C
 \n
 \nMemory Usage: $ram_pcent %"
 
+fzzl_lines=$(echo -e $echo_str | wc -l) ## dynamic line amount for the menu
+
 fuzzel_cmd(){
     fuzzel --dmenu \
 	--border-color=7878eeff \
 	--anchor=top-left \
-	--lines=23 \
-	--line-height=16 \
+	--lines=$fzzl_lines \
+l	--line-height=16 \
 	--width=36%
 } ## fuzzel menu config
 
@@ -53,22 +55,27 @@ run_fuzzel(){
 	echo -e $echo_str | fuzzel_cmd
 } ## choices to list in fuzzel
 
-fzzl_choice="$(run_fuzzel)" ## start of script execution
+fzzl_choice="$(run_fuzzel)" ## start of script execution ## change command/s accrodingly
 case ${fzzl_choice} in
-    "> Audio ↴ ")
+    "> Audio ⌄ ")
         ## Spawn Audio Control Program , change accordingly
-        pavucontrol
+        flatpak run com.saivert.pwvucontrol
     ;;
     "$vol_out"|"$vol_out ")
         ## Mute / Unmute Speakers / Headset
         wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
     ;;
     "$vol_in"|"$vol_in ")
-        ## Mute / Unmute Micropphone
+        ## Mute / Unmute Microphone
         wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
     ;;
-    "> Network ↴ ")
+    "> Network ⌄ ")
         ## Spawn Network Manager Program , change accordingly
         nm-connection-editor
     ;;
+	"> Power Profile ⌄ ")
+		## Show all possible profiles
+		lines="$(tuned-adm list | wc -l)"
+		tuned-adm list | fuzzel --dmenu --width=150% --lines=$lines
+	;;
 esac
